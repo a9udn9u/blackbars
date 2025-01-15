3f9555b1949763a8ffec2b04857a45223d8b0775
Quan Gan <a9udn9u@gmail.com>
2025-01-14 21:28:24 -0800

    Fix build config for GitHub url prefix

--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,10 @@ import { defineConfig } from 'vite'
 import preact from '@preact/preset-vite'
 
 // https://vite.dev/config/
-export default defineConfig({
+export default defineConfig(({ command }) => ({
   plugins: [preact()],
+  base: command === 'build' ? '/blackbars/' : '/', // GitHub adds URL prefix
   build: {
     outDir: 'docs', // GitHub Pages root
   },
-})
+}))
